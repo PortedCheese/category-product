b39771471a7e1fa6415887d9075216d1a27a0eca
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-08-03 11:48:08 +0300

    change composer

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,16 +7,15 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
   public function boot()
   {
-    $this->publishes([
-            __DIR__ . '/../config/category-product.php' => config_path('category-product.php'),
-        ], 'category-product-config');
+      $this->publishes([
+          __DIR__ . '/config/category-product.php' => config_path('category-product.php'),
+      ], 'config');
   }
 
   public function register()
   {
-    $this->mergeConfigFrom(
-            __DIR__ . '/../config/category-product.php',
-            'category-product'
-        );
+      $this->mergeConfigFrom(
+          __DIR__ . '/config/category-product.php', 'category-product'
+      );
   }
 }
