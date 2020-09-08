6e31c14773a2e7b4c84e6a2b4f72b43363810f5f
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-09-08 17:09:01 +0300

    add favorites to user

--- src/Helpers/ProductFavoriteManager.php
+++ src/Helpers/ProductFavoriteManager.php
@@ -70,19 +70,77 @@ class ProductFavoriteManager
      */
     protected function saveFavorite(array $current)
     {
+        if (Auth::check()) {
+            $user = Auth::user();
+            $user->product_favorite = json_encode(array_values($current));
+            $user->save();
+        }
+
         $cookie = Cookie::make(self::COOKIE_NAME, json_encode(array_values($current)), 60*24*30);
         Cookie::queue($cookie);
-        // TODO: save for user.
         return $current;
     }
 
+    /**
+     * Найти избранное.
+     *
+     * @return array|string|null
+     */
     protected function findCurrentFavorite()
     {
         $favorite = Cookie::get(self::COOKIE_NAME, "[]");
         $favorite = array_values(json_decode($favorite));
-        if (empty($favorite) && Auth::check()) {
-            // TODO: get from user.
+        return $this->checkUserFavorite($favorite);
+    }
+
+    /**
+     * Если записанное отличается от куки, сохранить.
+     *
+     * @param $current
+     * @return array
+     */
+    protected function checkUserFavorite($current)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            if (! empty($user->product_favorite)) {
+                $userData = json_decode($user->product_favorite);
+                if ($this->checkDiffInArrays($userData, $current)) {
+                    $current = $this->mergeFavorite($current, $userData);
+                    $current = $this->saveFavorite($current);
+                }
+            }
         }
-        return $favorite;
+        return array_values($current);
+    }
+
+    /**
+     * Отличия в массивах.
+     *
+     * @param $first
+     * @param $second
+     * @return bool
+     */
+    protected function checkDiffInArrays($first, $second)
+    {
+        foreach ($first as $item) {
+            if (in_array($item, $second)) return true;
+        }
+        foreach ($second as $item) {
+            if (in_array($item, $first)) return true;
+        }
+        return false;
+    }
+
+    /**
+     * Объеденить избранное.
+     *
+     * @param $current
+     * @param $new
+     * @return array
+     */
+    protected function mergeFavorite($current, $new)
+    {
+        return array_unique(array_merge($current, $new));
     }
 }
