6e3e5df93f1669b1856dff848ef7a6d8f798e487
Vladimir <vladimirpeshkov5dev@yandex.ru>
2020-10-13 14:51:43 +0300

    clear product cache after update label

--- src/Observers/ProductLabelObserver.php
+++ src/Observers/ProductLabelObserver.php
@@ -2,10 +2,25 @@
 
 namespace PortedCheese\CategoryProduct\Observers;
 
+use App\Product;
 use App\ProductLabel;
 
 class ProductLabelObserver
 {
+    /**
+     * Очистить кэш товаров при обновлении метки.
+     * 
+     * @param ProductLabel $label
+     */
+    public function updated(ProductLabel $label)
+    {
+        foreach ($label->products as $product) {
+            /**
+             * @var Product $product
+             */
+            $product->clearCache();
+        }
+    }
 
     /**
      * После удаления.
